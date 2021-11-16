673a9bf9421afef54c04c2b81f030515c4ae5e33
Witor Augusto <witorsantosaugusto@gmail.com>
2021-11-16 11:08:31 -0300

    protegendo a API com Passport

--- config/routes.js
+++ config/routes.js
@@ -5,22 +5,27 @@ module.exports = app => {
     app.post('/validateToken', app.api.auth.validateToken)
 
     app.route('/users')
+        .all(app.config.passport.authenticate())
         .post(app.api.user.save)
         .get(app.api.user.get)
 
     app.route('/users/:id')
+    .all(app.config.passport.authenticate())
         .put(app.api.user.save)
         .get(app.api.user.getById)
 
     app.route('/categories')
+    .all(app.config.passport.authenticate())
         .get(app.api.categories.get)
         .post(app.api.categories.save)
 
     app.route('/articles')
+    .all(app.config.passport.authenticate())
         .get(app.api.articles.get)
         .post(app.api.articles.save)
 
     app.route('/articles/:id')
+    .all(app.config.passport.authenticate())
         .get(app.api.articles.getById)
         .put(app.api.articles.save)
         .delete(app.api.articles.remove)
@@ -28,13 +33,16 @@ module.exports = app => {
 
     //Cuidado com ordem! Tem que vir antes de /categories/:id
     app.route('/categories/tree')
+    .all(app.config.passport.authenticate())
         .get(app.api.categories.getTree)
 
     app.route('/categories/:id')
+    .all(app.config.passport.authenticate())
         .get(app.api.categories.getById)
         .put(app.api.categories.save)
         .delete(app.api.categories.remove)
 
     app.route('/categories/:id/articles')
+    .all(app.config.passport.authenticate())
         .get(app.api.articles.getByCategory)
 }
